1f308f4349f08c60a1d27f407739aba1892e818b
Poorvik <poorvik1212@gmail.com>
2024-11-27 02:00:44 +0530

    complete

---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ListItem from './ListItem'
+
+const List = ({items}) => {
+  return (
+      <ul>
+        {items.map(item=>(<ListItem key={items.id} item={item}/>))}
+      </ul>
+  )
+}
+
+export default List
